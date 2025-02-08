0dbb0cd918b0b0a1ac9bf6a8953c13ed288ea900
mfp1401 <marcelofroespadilha@gmail.com>
2025-02-07 23:59:46 -0300

    Add files via upload

---
+++ OlaJava/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int x=10;
+        int y=5;
+        int z;
+
+        z=x+y;
+
+        System.out.println("Hello Java");
+
+        System.out.println("z");
+
+        System.out.println("O valor de z é "+z);
+    }
+}
